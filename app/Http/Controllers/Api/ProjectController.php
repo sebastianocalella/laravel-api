7601262e58d1b688a3441d6712d0e90b8ee50c60
Sebastiano Calella <sebastianocalella97@gmail.com>
2023-03-02 17:22:30 +0100

    order brojects in frontend orderBy modification_date

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -9,7 +9,7 @@ use Illuminate\Http\Request;
 class ProjectController extends Controller
 {
     public function index(){
-        $projects = Project::with('type','tecnologies')->paginate(6);
+        $projects = Project::with('type','tecnologies')->orderBy('modification_date', 'DESC')->paginate(6);
         return response()->json(['success' => true, 'results' => $projects]);
     }
 }
